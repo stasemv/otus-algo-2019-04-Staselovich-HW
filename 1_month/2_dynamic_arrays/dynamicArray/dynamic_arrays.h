3ee7537d82562aabcde602d98436f7c8fb26ebbe
Staselovich Semyon <>
2019-05-13 16:51:52 +0300

    task 1

--- 1_month/2_dynamic_arrays/dynamicArray/dynamic_arrays.h
+++ 1_month/2_dynamic_arrays/dynamicArray/dynamic_arrays.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <cstring>
+#include <stdexcept>
 
 template<class T> class clsDynArr
 {
@@ -10,6 +11,7 @@ protected :
     T *_array;
     int _size;
     virtual void resize() {}
+    virtual void decrsize() {}
 
 public:
     explicit clsDynArr() {
@@ -21,8 +23,8 @@ public:
     virtual void add(T item) {}
     virtual T get( int index) {}
 
-//    virtual void add_i(T item, int index); // + shift to tail
-//    virtual T remove(int index);
+    virtual void add_i(T item, int index) {} // + shift to tail
+    virtual T remove(int index) {}
 };
 
 template<class T> class clsNoobDynArr : public clsDynArr<T>
@@ -31,10 +33,20 @@ protected :
     void resize() {
         T *newArr = new T[clsDynArr<T>::_size + 1];
         memcpy(newArr, clsDynArr<T>::_array, clsDynArr<T>::_size*sizeof(T));
-        clsDynArr<T>::_size++;
         if(clsDynArr<T>::_array)
-            delete clsDynArr<T>::_array;
+            delete[] clsDynArr<T>::_array;
+        clsDynArr<T>::_size++;
         clsDynArr<T>::_array = newArr;
+    }
+    void decrsize() {
+        if(clsDynArr<T>::_size > 0) {
+            T *newArr = new T[clsDynArr<T>::_size - 1];
+            clsDynArr<T>::_size--;
+            memcpy(newArr, clsDynArr<T>::_array, clsDynArr<T>::_size*sizeof(T));
+            if(clsDynArr<T>::_array)
+                delete[] clsDynArr<T>::_array;
+            clsDynArr<T>::_array = newArr;
+        }
     }
 
 public:
@@ -54,6 +66,29 @@ public:
             return clsDynArr<T>::_array[index];
         return T();
     }
+    void add_i(T item, int index) {
+        if((index >= 0) && (index <= clsDynArr<T>::_size)) {
+            int tail = clsDynArr<T>::_size - index;
+            resize();
+            memcpy(&(clsDynArr<T>::_array[index+1]),
+                   &(clsDynArr<T>::_array[index]),
+                   tail*sizeof(T));
+            clsDynArr<T>::_array[index] = item;
+        }
+    }
+    virtual T remove(int index) {
+        if((index >= 0) && (index < clsDynArr<T>::_size)) {
+            T item = get(index);
+            int tail = clsDynArr<T>::_size - index - 1;
+            memcpy(&clsDynArr<T>::_array[index],
+                   &clsDynArr<T>::_array[index+1],
+                   tail*sizeof(T));
+            decrsize();
+            return item;
+        }
+        return T();
+    }
+
     T* getPtr(int index) {
         if((index >= 0) && (index < clsDynArr<T>::_size))
             return &(clsDynArr<T>::_array[index]);
@@ -73,6 +108,17 @@ private:
         if(clsDynArr<T>::_array)
             delete clsDynArr<T>::_array;
         clsDynArr<T>::_array = newArr;
+    }
+    void decrsize() {
+        if(clsDynArr<T>::_size <= _capacity - _block) {
+//            printf("remove block\n");
+            _capacity -= _block;
+            T *newArr = new T[_capacity];
+            memcpy(newArr, clsDynArr<T>::_array, _capacity*sizeof(T));
+            if(clsDynArr<T>::_array)
+                delete clsDynArr<T>::_array;
+            clsDynArr<T>::_array = newArr;
+        }
     }
 
 public:
@@ -98,6 +144,34 @@ public:
             return clsDynArr<T>::_array[index];
         return T();
     }
+
+    void add_i(T item, int index) {
+        if((index >= 0) && (index <= clsDynArr<T>::_size)) {
+            if(clsDynArr<T>::_size == _capacity)
+                resize();
+//            printf(" add %i to %i place\n", (int)item, index);
+            int tail = clsDynArr<T>::_size - index;
+            memcpy(&(clsDynArr<T>::_array[index+1]),
+                   &(clsDynArr<T>::_array[index]),
+                   tail*sizeof(T));
+            clsDynArr<T>::_array[index] = item;
+            clsDynArr<T>::_size++;
+        }
+    }
+    virtual T remove(int index) {
+        if((index >= 0) && (index < clsDynArr<T>::_size)) {
+            T item = get(index);
+//            printf("removed %i\n", (int)item);
+            int tail = clsDynArr<T>::_size - index - 1;
+            memcpy(&clsDynArr<T>::_array[index],
+                   &clsDynArr<T>::_array[index+1],
+                    tail*sizeof(T));
+            clsDynArr<T>::_size--;
+            decrsize();
+            return item;
+        }
+        return T();
+    }
 };
 
 template<class T> class clsFactorArr : public clsDynArr<T>
@@ -105,6 +179,7 @@ template<class T> class clsFactorArr : public clsDynArr<T>
 private:
     int _factor;
     int _capacity;
+    int _init_length;
     void resize() {
         int extention = _capacity * _factor / 100;
         T *newArr = new T[_capacity + extention];
@@ -114,9 +189,23 @@ private:
             delete clsDynArr<T>::_array;
         clsDynArr<T>::_array = newArr;
     }
-
+    void decrsize() {
+        int residue = (double)_capacity
+                * ((double)_factor / ((double)_factor + 100.0));
+        if((clsDynArr<T>::_size <= _capacity - residue)
+                && (_capacity - residue > _init_length)) {
+//            printf("remove block\n");
+            _capacity -= residue;
+            T *newArr = new T[_capacity];
+            memcpy(newArr, clsDynArr<T>::_array, _capacity*sizeof(T));
+            if(clsDynArr<T>::_array)
+                delete clsDynArr<T>::_array;
+            clsDynArr<T>::_array = newArr;
+        }
+    }
 public:
     clsFactorArr(int factor, int init_length) {
+        _init_length - init_length;
         clsDynArr<T>::_array = new T[init_length];
         _factor = factor;
         clsDynArr<T>::_size = 0;
@@ -138,6 +227,33 @@ public:
             return clsDynArr<T>::_array[index];
         return T();
     }
+    void add_i(T item, int index) {
+        if((index >= 0) && (index <= clsDynArr<T>::_size)) {
+            if(clsDynArr<T>::_size == _capacity)
+                resize();
+//            printf(" add %i to %i place\n", (int)item, index);
+            int tail = clsDynArr<T>::_size - index;
+            memcpy(&(clsDynArr<T>::_array[index+1]),
+                   &(clsDynArr<T>::_array[index]),
+                   tail*sizeof(T));
+            clsDynArr<T>::_array[index] = item;
+            clsDynArr<T>::_size++;
+        }
+    }
+    virtual T remove(int index) {
+        if((index >= 0) && (index < clsDynArr<T>::_size)) {
+            T item = get(index);
+//            printf("removed %i\n", (int)item);
+            int tail = clsDynArr<T>::_size - index - 1;
+            memcpy(&clsDynArr<T>::_array[index],
+                   &clsDynArr<T>::_array[index+1],
+                    tail*sizeof(T));
+            clsDynArr<T>::_size--;
+            decrsize();
+            return item;
+        }
+        return T();
+    }
 };
 
 template<class T> class clsMatrixArr : public clsDynArr<T>
@@ -149,6 +265,14 @@ private:
     void resize() {
         _matrix->add(clsBlockArr<T>(_vector));
         _capacity += _vector;
+    }
+    void decrsize() {
+        if(clsDynArr<T>::_size <= _capacity - _vector) {
+            printf("remove block\n");
+            int last_idx = _matrix->size() - 1;
+            _capacity -= _vector;
+            _matrix->remove(last_idx);
+        }
     }
 
 public:
@@ -175,6 +299,42 @@ public:
         if((index >= 0) && (index < clsDynArr<T>::_size)) {
             clsBlockArr<T> block = _matrix->get(index / _vector);
             return block.get(index % _vector);
+        }
+        return T();
+    }
+    void add_i(T item, int index) {
+        if((index >= 0) && (index <= clsDynArr<T>::_size)) {
+            if(clsDynArr<T>::_size == _capacity)
+                resize();
+            printf(" add %i to %i place\n", (int)item, index);
+
+            int row = index / _vector;
+            for(int i=clsDynArr<T>::_size / _vector; i > row; --i) {
+                int last_idx = _matrix->getPtr(i-1)->size() - 1;
+                T val = _matrix->getPtr(i-1)->get(last_idx);
+                _matrix->getPtr(i)->add_i(val, 0);
+                _matrix->getPtr(i-1)->remove(last_idx);
+            }
+            _matrix->getPtr(row)->add_i(item, index % _vector);
+            clsDynArr<T>::_size++;
+        }
+    }
+    virtual T remove(int index) {
+        if((index >= 0) && (index < clsDynArr<T>::_size)) {
+            T item = get(index);
+            printf("removed %i (idx = %i)\n", (int)item, index);
+
+            int row = index / _vector;
+            _matrix->getPtr(row)->remove(index % _vector);
+            clsDynArr<T>::_size--;
+            int rows = clsDynArr<T>::_size / _vector;
+            for(int i=row; i < clsDynArr<T>::_size / _vector; ++i) {
+                T val = _matrix->getPtr(i+1)->get(0);
+                _matrix->getPtr(i)->add(val);
+                _matrix->getPtr(i+1)->remove(0);
+            }
+            decrsize();
+            return item;
         }
         return T();
     }
--- 1_month/2_dynamic_arrays/dynamicArray/use_arrays.cpp
+++ 1_month/2_dynamic_arrays/dynamicArray/use_arrays.cpp
@@ -4,51 +4,54 @@
 #include "time.h"
 #include "dynamic_arrays.h"
 
+#define _PRINT_ARRAYS_ 0
+
+template<class T>
+void printArray(clsDynArr<T> *arr);
 template<class T>
 void testAddArrays(const char *name, clsDynArr<T> *arr, int amount);
+template<class T>
+void testAddIArrays(const char *name, clsDynArr<T> *arr, int amount);
+template<class T>
+void testRemoveIArrays(const char *name, clsDynArr<T> *arr, int amount);
 
 void useAllArrays()
 {
 //#define _type int
 #define _type double
-    int total = 100000;
-    clsNoobDynArr<_type> _type_noob_dyn_arr;
-    testAddArrays<_type>("noob", &_type_noob_dyn_arr, total);
+    int total = 1000;
+//    clsNoobDynArr<_type> _type_noob_dyn_arr;
+//    testAddArrays<_type>("noob", &_type_noob_dyn_arr, total);
+//    testAddIArrays<_type>("noob", &_type_noob_dyn_arr, total/100);
+//    testRemoveIArrays<_type>("noob", &_type_noob_dyn_arr, total/100);
 
-    clsBlockArr<_type> _type_block_arr;
-    testAddArrays<_type>("block", &_type_block_arr, total);
-    testAddArrays<_type>("block_100", new clsBlockArr<_type>(100), total);
+//    clsBlockArr<_type> _type_block_arr;
+//    testAddArrays<_type>("block", &_type_block_arr, total);
+//    testAddIArrays<_type>("block", &_type_block_arr, total/100);
+//    testRemoveIArrays<_type>("block", &_type_block_arr, total/100);
+//    testAddArrays<_type>("block_100", new clsBlockArr<_type>(100), total);
 
-    clsFactorArr<_type> _type_factor_arr;
-    testAddArrays<_type>("factor", &_type_factor_arr, total);
+//    clsFactorArr<_type> _type_factor_arr;
+//    testAddArrays<_type>("factor", &_type_factor_arr, total);
+//    testAddIArrays<_type>("factor", &_type_factor_arr, total*0.5);
+//    testRemoveIArrays<_type>("factor", &_type_factor_arr, total*0.33 + 1);
 
     clsMatrixArr<_type> _type_matr_arr(10);
     testAddArrays<_type>("matr", &_type_matr_arr, total);
+    testAddIArrays<_type>("matr", &_type_matr_arr, total/100);
+    testRemoveIArrays<_type>("matr", &_type_matr_arr, total/100);
 }
 
 template<class T>
 void testAddArrays(const char *name, clsDynArr<T> *arr, int amount)
 {
-#define _PRINT_ARRAYS_ 0
-#if _PRINT_ARRAYS_
-    int printCount = 0;
-#endif
     clock_t c_start, c_finish;
     c_start = clock();
 
-    for(int i=0; i < amount; ++i) {
-//        T *added = new T((_type)i);
-//        arr->add(*added);
-        arr->add(T((_type)(i+1)));
-#if _PRINT_ARRAYS_
-        _type val = arr->get(i);
-        printf("%i\t", (int)val);
-    if(++printCount >= 10) {
-        printf("\n");
-        printCount = 0;
-    }
-#endif
-    }
+    for(int i=0; i < amount; ++i)
+        arr->add(T((_type)(i)));
+
+    printArray(arr);
 
     c_finish = clock();
     clock_t delta = c_finish-c_start;
@@ -57,3 +60,62 @@ void testAddArrays(const char *name, clsDynArr<T> *arr, int amount)
     printf("%s add time = %f\n", name, operatingTimeFull);
 }
 
+template<class T>
+void testAddIArrays(const char *name, clsDynArr<T> *arr, int amount)
+{
+    clock_t c_start, c_finish;
+    c_start = clock();
+
+    for(int i=0; i < amount; ++i) {
+        int amount = arr->size();
+        int idx = rand() % amount;
+        arr->add_i(T((_type)(amount+1)), idx);
+    }
+
+    printArray(arr);
+
+    c_finish = clock();
+    clock_t delta = c_finish-c_start;
+    printf("%s add_i clocks = %li\n", name, delta);
+    double operatingTimeFull = delta*1.0/CLOCKS_PER_SEC;
+    printf("%s add_i time = %f\n", name, operatingTimeFull);
+}
+
+template<class T>
+void testRemoveIArrays(const char *name, clsDynArr<T> *arr, int amount)
+{
+    clock_t c_start, c_finish;
+    c_start = clock();
+
+    for(int i=0; i < amount; ++i) {
+        int amount = arr->size();
+        int idx = rand() % amount;
+        T item = arr->remove(idx);
+    }
+
+    printArray(arr);
+
+    c_finish = clock();
+    clock_t delta = c_finish-c_start;
+    printf("%s remove clocks = %li\n", name, delta);
+    double operatingTimeFull = delta*1.0/CLOCKS_PER_SEC;
+    printf("%s remove time = %f\n", name, operatingTimeFull);
+}
+
+template<class T>
+void printArray(clsDynArr<T> *arr)
+{
+#if _PRINT_ARRAYS_
+    int printCount = 0;
+    int _num = arr->size();
+    for(int i=0; i < _num; ++i) {
+        _type val = arr->get(i);
+        printf("%i\t", (int)val);
+        if(++printCount >= 10) {
+            printf("\n");
+            printCount = 0;
+        }
+    }
+    printf("\n");
+#endif
+}
