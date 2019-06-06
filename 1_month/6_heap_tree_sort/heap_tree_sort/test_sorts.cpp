e10ffe75cca2401fb4094b6106235e64417ddbad
Staselovich Semyon <>
2019-06-06 15:51:17 +0300

    add simple tim sort

--- 1_month/6_heap_tree_sort/heap_tree_sort/test_sorts.cpp
+++ 1_month/6_heap_tree_sort/heap_tree_sort/test_sorts.cpp
@@ -17,11 +17,15 @@ void test_sorts()
 //    heapSort<double>(array, dim);
 //    printHeap<double>(array, dim, fout);
 
-    printArray<double>(array, dim, fout, "origin:");
+//    printArray<double>(array, dim, fout, "origin");
 //    mergeSort<double>(array, dim);
 //    printArray<double>(array, dim, fout, "sorted");
-    quickSort<double>(array, 0, dim);
-    printArray<double>(array, dim, fout, "sorted");
+//    quickSort<double>(array, 0, dim);
+//    printArray<double>(array, dim, fout, "sorted");
+    int intArr[] = { 1, 3, 6, 9, 10, 12, 4, 2, 0, 5, 7, 8, 19, 15, 13 };
+    int intArrDim = sizeof(intArr)/sizeof(intArr[0]);
+    printArray<int>(intArr, intArrDim, fout, "origin");
+    timSort<int>(intArr, intArrDim);
 
     delete array;
 }
